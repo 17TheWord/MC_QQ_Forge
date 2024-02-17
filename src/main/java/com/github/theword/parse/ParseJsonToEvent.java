385f21916f6ed7b95d84523246e609187ae8a750
17TheWord <17theword@gmail.com>
2024-02-17 23:24:58 +0800

    适配悬浮事件中ShowItem、ShowEntity

--- src/main/java/com/github/theword/parse/ParseJsonToEvent.java
+++ src/main/java/com/github/theword/parse/ParseJsonToEvent.java
@@ -1,10 +1,16 @@
 package com.github.theword.parse;
 
 import com.github.theword.returnBody.returnModle.MyBaseComponent;
+import com.github.theword.returnBody.returnModle.MyHoverEntity;
+import com.github.theword.returnBody.returnModle.MyHoverItem;
 import com.github.theword.returnBody.returnModle.MyTextComponent;
 import net.minecraft.network.chat.*;
 import net.minecraft.network.chat.contents.LiteralContents;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.client.event.RenderHighlightEvent;
 
 import java.util.List;
 
@@ -44,24 +50,29 @@ public class ParseJsonToEvent {
                 ClickEvent clickEvent = new ClickEvent(tempAction, myTextComponent.getClickEvent().getValue());
                 style = style.withClickEvent(clickEvent);
             }
-            // TODO 悬浮事件待完善
-            if (myTextComponent.getHoverEvent() != null) {
+            // TODO 悬浮事件待测试
+            if (myTextComponent.getHoverEvent() != null && myTextComponent.getHoverEvent().getBaseComponentList() != null && myTextComponent.getHoverEvent().getBaseComponentList().size() > 0) {
                 HoverEvent hoverEvent = null;
                 switch (myTextComponent.getHoverEvent().getAction()) {
-                    case "show_text":
-                        if (myTextComponent.getHoverEvent().getBaseComponentList() != null && myTextComponent.getHoverEvent().getBaseComponentList().size() > 0) {
-                            MutableComponent textComponent = parseMessages(myTextComponent.getHoverEvent().getBaseComponentList());
-                            hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, textComponent);
+                    case "show_text" -> {
+                        MutableComponent textComponent = parseMessages(myTextComponent.getHoverEvent().getBaseComponentList());
+                        hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, textComponent);
+                    }
+                    case "show_item" -> {
+                        MyHoverItem myHoverItem = myTextComponent.getHoverEvent().getItem();
+                        ItemStack itemStack = new ItemStack(Item.byId(myHoverItem.getId()));
+                        itemStack.setCount(myHoverItem.getCount());
+                        HoverEvent.ItemStackInfo itemStackInfo = new HoverEvent.ItemStackInfo(itemStack);
+                        hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_ITEM, itemStackInfo);
+                    }
+                    case "show_entity" -> {
+                        MyHoverEntity myHoverEntity = myTextComponent.getHoverEvent().getEntity();
+                        MutableComponent nameComponent = parseMessages(myHoverEntity.getName());
+                        HoverEvent.EntityTooltipInfo entity = HoverEvent.EntityTooltipInfo.create(nameComponent);
+                        if (entity != null) {
+                            hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_ENTITY, entity);
                         }
-                        break;
-                    case "show_item":
-//                            hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_ITEM, new Item());
-                        break;
-                    case "show_entity":
-//                            hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_ENTITY, new Entity());
-                        break;
-                    default:
-                        break;
+                    }
                 }
                 style = style.withHoverEvent(hoverEvent);
             }
